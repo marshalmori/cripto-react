ff75ac1e79be5f44dafe9bad7a470946ee9a5284
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-01-18 16:09:45 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import styled from "@emotion/styled";
+
+const Heading = styled.h1`
+  font-family: "Lato", sans-serif;
+  color: #fff;
+`;
+
+const App = () => {
+  return <Heading>App js</Heading>;
+};
+
+export default App;

f22a3f6b6c86cab7c603d5f4d7db341dc04bc2ef
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-01-23 14:03:26 -0300

    Criando estilos para o componente Resultado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,26 +1,60 @@
 import styled from "@emotion/styled";
 
+const Contenedor = styled.div`
+  color: #fff;
+  font-family: "Lato", sans-serif;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 30px;
+`;
+
+const Imagen = styled.img`
+  display: block;
+  width: 120px;
+`;
+
+const Texto = styled.p`
+  font-size: 18px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+const Precio = styled.p`
+  font-size: 24px;
+  span {
+    font-weight: 700;
+  }
+`;
+
 const Resultado = ({ resultado }) => {
   const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } =
     resultado;
   return (
-    <div>
-      <p>
-        Preço: <span>{PRICE}</span>
-      </p>
-      <p>
-        Maior alta: <span>{HIGHDAY}</span>
-      </p>
-      <p>
-        Menor baixa: <span>{LOWDAY}</span>
-      </p>
-      <p>
-        Variação das 24h: <span>{CHANGEPCT24HOUR}</span>
-      </p>
-      <p>
-        Última atualização: <span>{LASTUPDATE}</span>
-      </p>
-    </div>
+    <Contenedor>
+      <Imagen
+        src={`https://cryptocompare.com/${IMAGEURL}`}
+        alt="Imagem da criptomoeda"
+      />
+      <div>
+        <Precio>
+          Preço: <span>{PRICE}</span>
+        </Precio>
+        <Texto>
+          Maior alta: <span>{HIGHDAY}</span>
+        </Texto>
+        <Texto>
+          Menor baixa: <span>{LOWDAY}</span>
+        </Texto>
+        <Texto>
+          Variação das 24h: <span>{CHANGEPCT24HOUR}</span>
+        </Texto>
+        <Texto>
+          Última atualização: <span>{LASTUPDATE}</span>
+        </Texto>
+      </div>
+    </Contenedor>
   );
 };
 
